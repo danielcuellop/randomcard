d723c1c12204da6739331d8fcd8ebc60a4200dd1
danielcuellop <franciscocuello86@gmail.com>
2023-03-31 00:53:49 +0000

    primer Push

--- src/app.js
+++ src/app.js
@@ -1,11 +1,46 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pintafinal = pintaAleatoria();
+  let pintaRandom = document.getElementsByTagName("h3");
+  let numeroRandom = numeroAleatorio();
+  let valorRandom = document.getElementsByTagName("h1")[0];
+  let color = colorCard(pintaRandom);
+  pintaRandom[0].textContent = pintafinal;
+  pintaRandom[1].textContent = pintafinal;
+  valorRandom.textContent = numeroRandom;
+  pintaRandom[0].style.color = color;
+  pintaRandom[1].style.color = color;
+  valorRandom.style.color = color;
 };
+function pintaAleatoria() {
+  let pinta = ["♦", "♥", "♠", "♣"];
+  let positionP = Math.floor(Math.random() * 4);
+  return pinta[positionP];
+}
+function numeroAleatorio() {
+  let numero = [
+    "A",
+    "K",
+    "Q",
+    "J",
+    "10",
+    "9",
+    "8",
+    "7",
+    "6",
+    "5",
+    "4",
+    "3",
+    "2"
+  ];
+  let positionN = Math.floor(Math.random() * 13);
+  return numero[positionN];
+}
+
+function colorCard(x) {
+  if (x === "♦" || x === "♥" ) {
+    return "#f00";
+  }
+  else{
+    return "#00";
+  }
+}
